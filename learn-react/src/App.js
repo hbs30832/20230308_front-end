9e9a156e01c5ca225e6c85c25a62cd6ab46bf448
hbs30832 <hbs9312@gmail.com>
2023-03-27 12:20:32 +0900

    4. JSX에 JavaScript 값 포함시키기

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,38 +1,36 @@
-export default function TodoList() {
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+}
+
+export default function Avatar() {
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+  const description = "Gregorio Y. Zara";
+  const title = "React";
   return (
-    // This doesn't quite work!
     <>
-      <h1>Hedy Lamarr's Todos</h1>
-      <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        class="photo"
-      />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul>
+      <h1
+        style={{
+          color: "red",
+          fontWeight: "lighter",
+        }}
+      >
+        Hello {title}!
+      </h1>
+      <h2>오늘은 {formatDate(today)}</h2>
+      <img className="avatar" src={avatar} alt={description} />
     </>
   );
 }
 
 /* 
-  JSX(JavaScript and XML)
-    - JavaScript 마크업 언어를 포함시키기 위한 확장된 문법.
-      => JS지만 HTML 구조를 한눈에 알아보기 쉽다.
-    - JSX는 React 개발을 도와주는 하나의 도구이다. 즉, React 별개이다.
-      => create-react-app를 통해 프로젝트를 생성하면 Babel이라는 도구가 포함된다.
+  JSX 안에 JavaScript 값(표현식) 포함 시키기.
 
-  JSX 지켜야될 문법
+    - JSX안에서 {}안에 JavaScript 표현식을 작성하면 동적인 값을 사용할 수 있다.
+      => 변수, 연산, 함수 호출, 객체 리터럴 등 다양한 JavaScript 값을 포함 시킬 수 있다.
+      => 값으로 평가되는 표현식이여야한다. 즉, if문, for문 등은 사용할 수 없다.
 
-    1. 컴포넌트는 무조건 하나의 루트 요소를 반환해야한다.
-        => 불필요한 요소를 작성하기 싫다면, <></> 빈 태그를 사용하면 된다. 이를 Fragment라고 한다.
-
-    2. <img>와 같은 셀프 클로징 태그에는 /로 닫아주어야한다.
-        => ex) <img src=""> => <img src="" />
-    
-    3. JSX에서는 거의 대부분 카멜케이스를 사용한다.
-        => 작성한 속성(Attribute) 등이 결국에는 JS 객체로 변환되기 때문.
-        => 요소에 클래스를 부여할 때 class 대신 className을 사용한다.
+    - style 속성을 통해 스타일을 지정할 때는 객체를 전달해야한다.
+      => {{}} 형태로 작성한다. 겉의 {}는 JavaScript 값임을 명시하고, 안쪽의 {}를 객체 리터럴이다.
 */
