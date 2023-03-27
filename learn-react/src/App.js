2fc13d318fb2cd48ed75d855f0c1df45ceece938
hbs30832 <hbs9312@gmail.com>
2023-03-27 15:05:16 +0900

    6. children

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,19 +1,12 @@
-// 첫번째 매개변수로 props 객체가 넘어온다. {}를 넣으면 비구조화 할당이 된다.
-function Avatar({ person, size = 200 }) {
-  return (
-    <img
-      className="avatar"
-      src={`https://i.imgur.com/${person.imageId}.jpg`}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
+import Avatar from "./Avatar.jsx";
+
+function Card({ children }) {
+  return <div className="card">{children}</div>;
 }
 
 export default function Profile() {
   return (
-    <div>
+    <Card>
       <Avatar
         size={100}
         person={{
@@ -21,30 +14,12 @@ export default function Profile() {
           imageId: "YfeOqp2",
         }}
       />
-      <Avatar
-        size={80}
-        person={{
-          name: "Aklilu Lemma",
-          imageId: "OKS67lh",
-        }}
-      />
-      <Avatar
-        person={{
-          name: "Lin Lanying",
-          imageId: "1bX5QH6",
-        }}
-      />
-    </div>
+    </Card>
   );
 }
 
 /* 
-  Props
-
-    - 같은 컴포넌트를 여러번 사용할 때 내용 등을 다르게 렌더링할 수 있다.
-    - 컴포넌트에 프로퍼티들을 나열하면 자식 컴포넌트의 첫번째 매개변수로 props 객체가 전달된다.
-      => key-value 형태로 전달된다.
-      => 매개변수에 {}를 사용하면 비구조화 할당이 되어 코드를 간소화할 수 있다.
-    - 매개변수 기본값 문법을 사용하여 프로퍼티가 전달되지 않았을 때 기본값을 적용할 수 있다.
-    - 부모 컴포넌트가 받은 props를 자식 컴포넌트한테 모두 전달할 때는 펼침 연산자를 통해서 간단하게 전달할 수 있다.
+  children
+    - 컴포넌트의 여는 태그와 닫는 태그 사이의 내용은 children이라는 약속된 프로퍼티로 전달된다.
+      => 원하는 위치 작성하여 출력할 수 있다.
 */
