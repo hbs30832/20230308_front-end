82e2232c269a55e10327d616d7d4d3e4cb2ed567
hbs30832 <hbs9312@gmail.com>
2023-03-28 15:01:11 +0900

    12. 여러개의 input 상태 관리

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,9 +1,10 @@
+import Input from "./components/Input";
 import Pointer from "./components/Pointer";
 
 export default function App() {
   return (
     <div>
-      <Pointer />
+      <Input />
     </div>
   );
 }
