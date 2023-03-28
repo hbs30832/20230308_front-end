3508af169476aade3a06fd4bcce831738743ff02
hbs30832 <hbs9312@gmail.com>
2023-03-28 10:38:39 +0900

    9. 이벤트 처리

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,27 +1,25 @@
-import { people } from "./data";
-import { getImageUrl } from "./utils";
-
-// Subrahmanyan Chandrasekhar : astrophysicist
-export default function List() {
-  const chemists = people.filter((person) => person.profession === "chemist");
-  const listItem = chemists.map((person) => (
-    <li key={person.id} style={{ display: "flex" }}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <div>
-        <b>{person.name}</b> : {person.profession}
-        <p>{person.accomplishment}</p>
-      </div>
-    </li>
-  ));
-  return <ul>{listItem}</ul>;
+export default function Toolbar() {
+  // 검색 중... => handleSubmit 함수 작성하기. 새로고침 x.
+  function handleSubmit(e) {
+    e.preventDefault(); // 요소의 기본 기능을 실행하지
+    alert("검색 중...");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input />
+      <button>검색</button>
+    </form>
+  );
 }
 
 /* 
-  배열 렌더링하기
-    - JSX에 배열을 포함시키면 배열에 있는 아이템들을 렌더링할 수 있다.
-    - 데이터를 배열로 관리하면서 map()과 filter() 함수를 통해서 렌더링할 수 있다.
-      => map() : 데이터를 JSX 변환할 수 있다.
-      => filter() : 렌더링할 데이터를 필터링할 수 있다.
-    - 배열을 렌더링할 때는 JSX에 고유한 key값을 부여해야한다.
-      => React가 각 데이터를 구분해서 성능 최적화를 할 수 있다.
+  이벤트 처리
+    - 요소의 on 접두사로 시작하는 이벤트 속성에 함수를 전달한다.
+      => 속성명은 카멜케이스를 사용한다.
+      => 함수 자체를 전달해야 하고, 함수 호출을 하면 안된다.
+      => 인자를 전달하는 등 함수를 호출해야 하는 형태라면, 익명 함수 내에서 함수를 호출하면 된다.
+    - props 객체를 통해서 하위 컴포넌트에 전달할 수 있다.
+    - 하위 요소에서 상위 요소로 같은 이벤트는 전파된다.
+      => e.stopPropagation()을 사용하면 상위 요소로 이벤트 전파를 하지않는다.
+    - form 태그와 같이 태그의 특수한 기능을 실행하지 않고자 할 때는 e.preventDefault()를 사용한다.
 */
