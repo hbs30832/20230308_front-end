6367df637a41fd140a8e548b1d3b1c83a9191ac2
hbs30832 <hbs9312@gmail.com>
2023-03-28 12:18:38 +0900

    11. 객체로 상태 관리하기

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,9 +1,9 @@
-import Counter from "./components/Counter";
+import Pointer from "./components/Pointer";
 
 export default function App() {
   return (
     <div>
-      <Counter />
+      <Pointer />
     </div>
   );
 }
