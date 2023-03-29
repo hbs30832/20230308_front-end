62217f5f53a1070b93f46e2d1f2718ca4ab36728
hbs30832 <hbs9312@gmail.com>
2023-03-29 10:17:22 +0900

    TodoList, TodoInput, TodoHeader 컴포넌트 작성

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,30 +1,22 @@
 import { useState } from "react";
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+const initialState = [
+  { id: 1, text: "React 프로젝트 생성하기", done: true },
+  { id: 2, text: "컴포넌트 만들기", done: true },
+  { id: 3, text: "상태 관리하기", done: false },
+];
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(initialState);
 
-  const handleCount = () => {
-    setCount(count + 10);
-    setCount((c) => c + 1);
-    setCount((c) => c + 1);
-  };
   return (
     <div>
-      <h2>{count}</h2>
-      <button onClick={handleCount}>+3</button>
+      <TodoHeader />
+      <TodoInput />
+      <TodoList todos={todos} />
     </div>
   );
 }
-
-/* 
-  useState가 반환하는 set 함수에 컴포넌트의 상태값 넣어서 여러번 호출해도
-  현재 컴포넌트의 상태값은 동일하기 때문에 효과가 없다.
-
-  리액트는 이벤트 핸들러 내부의 코드가 모두 실행하고 마지막에 렌더링을 시킨다.
-    => 성능, 상태 관리가 제대로 되기 위해서.
-  
-  하나의 이벤트 핸들러에서 set 함수를 여러번 부르면 그것이 큐에 추가된다.
-
-  set 함수에 콜백함수를 전달하면 첫번째 인자로 실시간 상태값을 받는다.
-    => 차례대로 상태 업데이트가 되고 마지막 상태 업데이트가 끝나면 렌더링이 일어난다.
-*/
