326112dfe339c1e5b82058b6b361fa97a07cdbb0
hbs30832 <hbs9312@gmail.com>
2023-04-06 13:11:44 +0900

    영화사이트 사이드바 완성

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,11 +1,46 @@
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin : 0;
+    padding : 0;
+    box-sizing : border-box;
+  }
+
+  a {
+    color : inherit;
+    text-decoration : none;
+  }
+
+  li {
+    list-style : none;
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <ThemeProvider
+      theme={{
+        colors: {
+          main: "#3f556c",
+        },
+      }}
+    >
+      <GlobalStyle />
+      <Container>
+        <Sidebar />
+        <MainContent />
+      </Container>
+    </ThemeProvider>
   );
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  height: 100vh;
+`;
 
 export default App;
--- movie-list/src/components/MainContent.jsx
+++ movie-list/src/components/MainContent.jsx
@@ -1,5 +1,11 @@
+import styled from "styled-components";
+
 function MainContent() {
-  return <div>content</div>;
+  return <Container>content</Container>;
 }
+
+const Container = styled.div`
+  background-color: #eee;
+`;
 
 export default MainContent;
