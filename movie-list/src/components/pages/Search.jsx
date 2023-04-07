7af94f2244c80f0cbebf92d1d0f32686ab9ecd76
hbs30832 <hbs9312@gmail.com>
2023-04-07 17:38:04 +0900

    쿼리스트링 수정

--- movie-list/src/components/pages/Search.jsx
+++ movie-list/src/components/pages/Search.jsx
@@ -3,6 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import { tmdbAxios } from "../../api/tmdbAxios";
 import Poster from "../common/Poster";
 import styled from "styled-components";
+import qs from "qs";
 
 function Search() {
   // 쿼리스트링으로 넘어온 keyword 값 꺼내기
@@ -14,7 +15,11 @@ function Search() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await tmdbAxios.get("search/movie?query=" + query);
+      const { data } = await tmdbAxios.get("search/movie", {
+        params: {
+          query,
+        },
+      });
 
       setResults(data.results);
     };
@@ -22,13 +27,15 @@ function Search() {
     fetchData();
   }, [query]);
 
+  console.log(results);
+
   if (!results) return <div>로딩 중...</div>;
 
   return (
     <div>
       <PostList>
         {results.map((item) => (
-          <li>
+          <li key={item.id}>
             <Poster data={item} />
           </li>
         ))}
