61c7414745a1eae3df345eca6b27462e3076b7b7
hbs30832 <hbs9312@gmail.com>
2023-04-12 11:52:09 +0900

    RTK Query 시작

--- learn-redux/src/components/Movies.jsx
+++ learn-redux/src/components/Movies.jsx
@@ -9,15 +9,21 @@ function Movies() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMovies(1));
-  }, [dispatch]);
+    dispatch(fetchMovies(page));
+  }, [dispatch, page]);
 
   return (
     <div>
       <h1>Moives</h1>
       <p>page : {page}</p>
-      <button>+1</button>
-      <button>-1</button>
+      <button onClick={() => setPage(page + 1)}>+1</button>
+      <button
+        onClick={() => {
+          if (page > 1) setPage(page - 1);
+        }}
+      >
+        -1
+      </button>
       {isLoading ? (
         <div>로딩중</div>
       ) : (
