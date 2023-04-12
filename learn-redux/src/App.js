94e145fc8bc35cc363a1850aa6ff6c8844a57b76
hbs30832 <hbs9312@gmail.com>
2023-04-12 11:13:34 +0900

    Revert "Revert "redux thunk""
    This reverts commit 5e7d3300b55fb47bfa2463190d43b30005a698c9.

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,11 +1,11 @@
 import Counter from "./components/Counter";
+import Movies from "./components/Movies";
 import Todos from "./components/Todos";
 
 function App() {
   return (
     <div>
-      <Counter />
-      <Todos />
+      <Movies />
     </div>
   );
 }
