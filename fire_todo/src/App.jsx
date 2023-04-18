8be9ea6dd2db4f2bcb6622ed346d0f10207b5ab3
hbs30832 <hbs9312@gmail.com>
2023-04-18 18:16:03 +0900

    firebase 데이터 베이스 연동

---
+++ fire_todo/src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+  const handleSubmit = async () => {
+    await addDoc(collection(db, "todos"), {
+      text,
+      done: false,
+    });
+
+    fetchTodos();
+  };
+
+  const handleToggle = async (id, done, text) => {
+    await setDoc(doc(db, "todos", id), {
+      text,
+      done: !done,
+    });
+
+    fetchTodos();
+  };
+
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+
+    fetchTodos();
+  };
+
+  const fetchTodos = async () => {
+    const querySnapshot = await getDocs(collection(db, "todos"));
+    /* 
+      querySnapshot.docs : 배열
+        => id 값과 데이터 값이 따로 있기 때문에 하나로 합쳐준다
+    */
+    const newTodos = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setTodos(newTodos);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        <button onClick={handleSubmit}>등록</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.done && "line-through" }}
+          >
+            <span onClick={() => handleToggle(todo.id, todo.done, todo.text)}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleDelete(todo.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ fire_todo/src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYMwjzHXWq_SlQqW6nsM5-nRtID4ZDEn8",
+  authDomain: "todolist-69bd1.firebaseapp.com",
+  projectId: "todolist-69bd1",
+  storageBucket: "todolist-69bd1.appspot.com",
+  messagingSenderId: "762626152263",
+  appId: "1:762626152263:web:943898d38067391d7b9e62",
+  measurementId: "G-XXW10ET0P7",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
