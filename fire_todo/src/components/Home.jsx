ccde8ffe2f6295aece95908b8d6277220297013f
hbs30832 <hbs9312@gmail.com>
2023-04-19 17:27:05 +0900

    firebase 배포

--- fire_todo/src/components/Home.jsx
+++ fire_todo/src/components/Home.jsx
@@ -1,28 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useId, useState } from "react";
 import { auth, storage } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { ref, uploadBytes } from "firebase/storage";
+import uuid from "react-uuid";
 
 function Home() {
   const navigate = useNavigate();
 
   const [image, setImage] = useState(null);
 
+  const id = uuid();
+
+  console.log(id);
+
   const handleUploadImage = async () => {
     // 참조(파일이 저장될 경로)를 만든다.
-    const imageRef = ref(storage, `profile/${image.name}`);
+    const imageRef = ref(storage, `profile/${id}_${image.name}`);
 
     // 위에서 만든 참조와 파일을 인자로 전달한다.
     const result = await uploadBytes(imageRef, image);
 
-    navigate(`/profile?ref=${result.ref}`);
+    navigate(`/profile?name=${result.metadata.name}`);
   };
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
         // 로그인 되어 있음
         console.log(user);
+        // 전역 상태로 유저 정보 관리하기. => redux, recoil, Context API 등...
         if (!user.emailVerified) {
           alert("너 인증해야됨");
         }
--- fire_todo/src/components/Profile.jsx
+++ fire_todo/src/components/Profile.jsx
@@ -1,5 +1,31 @@
+import { getDownloadURL, ref } from "firebase/storage";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { auth, storage } from "../firebase";
+
 function Profile() {
-  return <div></div>;
+  // 쿼리 파라미터로 전달된 ref값 가져오기
+  const [searchParams] = useSearchParams();
+  const [profileUrl, setProfileUrl] = useState("");
+
+  const name = searchParams.get("name");
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      // 파일 참조값을 생성한다.
+      const imageRef = ref(storage, `profile/${name}`);
+      const url = await getDownloadURL(imageRef);
+      setProfileUrl(url);
+    };
+
+    fetchProfile();
+  }, [name]);
+  return (
+    <div>
+      <h1>프로필</h1>
+      <img src={profileUrl} alt="" style={{ width: 500 }} />
+    </div>
+  );
 }
 
 export default Profile;
